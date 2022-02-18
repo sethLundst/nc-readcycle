b452e0e0dc06a0214fba0de15792e1068b3d9531
Seth <sethlundstram@protonmail.com>
2022-02-18 14:30:03 +0000

    Fix file structure

--- components/App.js
+++ components/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import BarcodeScanner from "./BarcodeScanner";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>ReadCycle</Text>
       <StatusBar style="auto" />
+      <BarcodeScanner></BarcodeScanner>
     </View>
   );
 }
---
+++ components/BarcodeScanner.js
@@ -0,0 +1,3 @@
+export default function BarcodeScanner() {
+  return <h1>Here</h1>;
+}
