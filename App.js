f89e5c80938160c8af0b44929eb76a0999c35fec
Seth <sethlundstram@protonmail.com>
2022-02-21 12:22:44 +0000

    Add colour highlighting to nav bar

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 import HomeScreen from "./components/HomeScreen";
 import ListBookScreen from "./components/ListBookScreen";
@@ -12,7 +13,24 @@ const Tab = createBottomTabNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator initialRouteName="Home">
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === "Home") {
+              iconName = focused
+                ? "ios-information-circle"
+                : "ios-information-circle-outline";
+            } else if (route.name === "Settings") {
+              iconName = focused ? "ios-list-box" : "ios-list";
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "green",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
         <Tab.Screen
           name="HomeScreen"
           component={HomeScreen}
