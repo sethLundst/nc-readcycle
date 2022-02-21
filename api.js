83ec7ef1779445562d9c3e4e2efb79dda1b09572
Seth <sethlundstram@protonmail.com>
2022-02-21 09:54:35 +0000

    Add getBook function

--- api.js
+++ api.js
@@ -3,3 +3,13 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "https://www.googleapis.com",
 });
+
+// Function to get book data.
+let ISBN = 9780735611313;
+async function getBook() {
+  const response = await api.get(
+    `/books/v1/volumes?q=isbn:${ISBN}&key=AIzaSyAVVkhe8oG7Y5vOVfzbb4tiSNuq5r0mbhQ`
+  );
+  console.log(response.data.items[0].volumeInfo);
+}
+getBook(ISBN);
