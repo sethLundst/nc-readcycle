8860e053f1510f8edef58ce1c47efed727f6143b
kirstyrichmond <kirsty.richmond.abc@hotmail.co.uk>
2022-02-21 18:19:59 +0000

    created a two column book list hardcoded with books

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import BarcodeScanner from "./components/BarcodeScanner";
+import BookList from "./components/BookList";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>ReadCycle</Text>
       <StatusBar style="auto" />
-      <BarcodeScanner></BarcodeScanner>
+      {/* <BarcodeScanner></BarcodeScanner> */}
+      <BookList />
     </View>
   );
 }
@@ -15,8 +17,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "white",
+    backgroundColor: "#FFF",
     alignItems: "center",
     justifyContent: "center",
+
+    // alignItems: "center",
+    // justifyContent: "center",
   },
 });
