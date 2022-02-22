4749950b873c25e106876539cfa9c946639f3d14
apoteka1 <joseph.ackroyd.1991@gmail.com>
2022-02-22 22:37:31 +0000

    add validation

--- api.js
+++ api.js
@@ -5,11 +5,11 @@ export const api = axios.create({
 });
 
 // Function to get book data.
-let ISBN = 9780735611313;
-async function getBook() {
-  const response = await api.get(
-    `/books/v1/volumes?q=isbn:${ISBN}&key=AIzaSyAVVkhe8oG7Y5vOVfzbb4tiSNuq5r0mbhQ`
-  );
-  console.log(response.data.items[0].volumeInfo);
-}
-getBook(ISBN);
+// let ISBN = 9780735611313;
+// async function getBook() {
+//   const response = await api.get(
+//     `/books/v1/volumes?q=isbn:${ISBN}&key=AIzaSyAVVkhe8oG7Y5vOVfzbb4tiSNuq5r0mbhQ`
+//   );
+  
+// }
+// getBook(ISBN);
