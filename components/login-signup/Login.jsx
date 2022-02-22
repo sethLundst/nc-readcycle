75e695c0a9ac9017e9bec42d77948da0934a9bdb
apoteka1 <joseph.ackroyd.1991@gmail.com>
2022-02-22 21:22:06 +0000

    add validation

--- components/login-signup/Login.jsx
+++ components/login-signup/Login.jsx
@@ -1,19 +1,69 @@
 import { Text, View, StyleSheet, TextInput } from "react-native";
 import React, { useState } from "react";
 import { KeyboardAvoidingView, TouchableOpacity } from "react-native-web";
-import validate from "validator"
-import { handleSignUp } from "../../db/firestore";
+import validate from "validator";
+import { handleSignUp, checkEmail, validateUsername } from "../../db/firestore";
 
 const Login = () => {
-	const [email, changeEmail] = useState("");
-	const [password, changePassword] = useState("");
+	const [userObj, setUserObj] = useState({
+		email: "",
+		password: "",
+		location: {},
+		username: "",
+	});
+	const [isRegistering, setIsRegistering] = useState(false);
+	const [emailErr, setEmailErr] = useState("");
+	const [pwordErr, setPwordErr] = useState("");
+ 
+	const handleChange = (text, name) => {
+		setUserObj((curr) => {
+			return { ...curr, [name]: text };
+		});
+	};
 
-  const validateNewUser = ()=>{
-    if (validate.isEmail(email)){
-      handleSignUp(email, password)
-    }
-  }
-	
+	const validateEmail = async (email) => {
+		if (!email) {
+			setEmailErr("Please enter an email address");
+			return;
+		} else if (!validate.isEmail(email)) {
+			setEmailErr("Invalid email address.");
+			return;
+		}
+		return checkEmail(email).then((isValid) => {
+			return isValid ? setEmailErr("") : setEmailErr("Email address in use.");
+		});
+	};
+
+	const validatePassword = (pword) => {
+		const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{7,20}$/;
+
+		if (!pword) {
+			setPwordErr("Please enter a password");
+			return;
+		}
+
+		if (!regex.test(pword)) {
+			setPwordErr(
+				"Password must be 7-20 characters long\nwith at least one uppercase letter,\none lowercase letter and one number."
+			);
+		} else {
+			setPwordErr(setPwordErr(false));
+		}
+	};
+
+	const handleRegisterClick = () => {
+		validateEmail(userObj.email)
+			.then(() => {
+        console.log(pwordErr, emailErr, 1);
+				validatePassword(userObj.password);
+        console.log(pwordErr, emailErr, 2);
+			})
+			.then(() => {
+				if (!pwordErr && !emailErr) {
+					setIsRegistering(true);
+				}
+			});
+	};
 
 	return (
 		<View style={styles.container}>
@@ -24,18 +74,21 @@ const Login = () => {
 				<KeyboardAvoidingView behavior="padding" style={styles.inputContainer}>
 					<TextInput
 						placeholder="email"
-						value={email}
+						value={userObj.email}
 						type="emailAddress"
-						onChangeText={(text) => changeEmail(text)}
+						data="email"
+						onChangeText={(text) => handleChange(text, "email")}
 						style={styles.text_input}
 						keyboardType="email-address"></TextInput>
-
+					{emailErr ? <Text style={styles.err_msg}>{emailErr}</Text> : null}
 					<TextInput
 						placeholder="password"
-						value={password}
-						onChangeText={(text) => changePassword(text)}
+						value={userObj.password}
+						data="password"
+						onChangeText={(text) => handleChange(text, "password")}
 						style={styles.text_input}
 						secureTextEntry></TextInput>
+					{pwordErr ? <Text style={styles.err_msg}>{pwordErr}</Text> : null}
 				</KeyboardAvoidingView>
 
 				<View style={styles.buttonContainer}>
@@ -44,7 +97,7 @@ const Login = () => {
 					</TouchableOpacity>
 					<TouchableOpacity
 						style={styles.button}
-						onPress={() => validateNewUser()}>
+						onPress={() => handleRegisterClick()}>
 						<Text style={styles.buttonText}>Register.</Text>
 					</TouchableOpacity>
 				</View>
@@ -95,7 +148,7 @@ const styles = StyleSheet.create({
 		padding: 8,
 		margin: 8,
 		borderRadius: 3,
-		fontSize: "4.5vw",
+		fontSize: "5vw",
 
 		fontFamily: "Courier",
 		color: "midnightblue",
@@ -105,8 +158,8 @@ const styles = StyleSheet.create({
 		padding: 10,
 		margin: 8,
 		borderRadius: 3,
-    shadowColor: "midnightblue",
-    shadowOffset: { width: "-1.2vw", height: "-1.2vw" },
+		shadowColor: "midnightblue",
+		shadowOffset: { width: "-1.2vw", height: "-1.2vw" },
 	},
 	buttonContainer: {
 		marginTop: 16,
@@ -115,7 +168,12 @@ const styles = StyleSheet.create({
 		fontFamily: "Courier",
 		color: "midnightblue",
 		fontWeight: "bold",
-		fontSize: "4.5vw",
+		fontSize: "5vw",
+	},
+	err_msg: {
+		fontFamily: "Courier",
+		color: "deeppink",
+		fontSize: "4vw",
 	},
 });
 
