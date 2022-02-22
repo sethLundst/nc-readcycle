bec2969fd4d404b2f22876c7478da9badd74c0c2
Seth <sethlundstram@protonmail.com>
2022-02-22 11:13:15 +0000

    Add barcode scanner import

--- screens/BarcodeScannerScreen.js
+++ screens/BarcodeScannerScreen.js
@@ -1,10 +1,11 @@
 import * as React from "react";
 import { View, Text } from "react-native";
+import BarcodeScanner from "../components/BarcodeScanner";
 
 export default function BarcodeScannerScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Barcode Scanner Here</Text>
+      <BarcodeScanner />
     </View>
   );
 }
