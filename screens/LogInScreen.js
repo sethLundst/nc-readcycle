93b67c8278b4621c0303fa6d89d2016b7e7dace8
Seth <sethlundstram@protonmail.com>
2022-02-23 14:57:57 +0000

    Debug

--- screens/LogInScreen.js
+++ screens/LogInScreen.js
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { View, Text } from "react-native";
-import Login from "../components/login-signup/Login";
 
 export default function LogInScreen() {
   return (
