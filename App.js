adad17b92b53940f93c6fd9e37e00419a8e019ea
Seth <seth@Seths-MacBook-Pro.local>
2022-02-23 14:48:31 +0000

    Add LogIn/SignUp Stack navigator

--- App.js
+++ App.js
@@ -1,19 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import BarcodeScanner from "./components/BarcodeScanner";
-
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import BottomTabNavigator from "./navigation/TabNavigator";
-import Login from "./components/login-signup/login";
 
 function App() {
-	return (
-		<Login></Login>
-			// <NavigationContainer>
-			// 	<BottomTabNavigator></BottomTabNavigator>
-			// </NavigationContainer>
-		
-	);
+  return (
+    <NavigationContainer>
+      <BottomTabNavigator></BottomTabNavigator>
+    </NavigationContainer>
+  );
 }
 export default App;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,11 @@
 import * as React from "react";
 import { View, Text } from "react-native";
-import BookList from "../components/BookList"
+import BookList from "../components/BookList";
 
 export default function HomeScreen() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-     <BookList></BookList>
+    <View>
+      <BookList></BookList>
     </View>
   );
 }
