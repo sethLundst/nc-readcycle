43a755196dc3c95cedffe299b831916b2dc8274c
Seth <sethlundstram@protonmail.com>
2022-02-24 12:18:06 +0000

    Add postcode validation

--- db/firestore.js
+++ db/firestore.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { initializeApp } from "firebase/app";
 import { getDoc, getFirestore } from "firebase/firestore";
 import {
@@ -9,7 +10,11 @@ import {
   setDoc,
   doc,
 } from "firebase/firestore";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAmmM3CBTdIeuOY7KEXouW-SFAHeyA_Ums",
@@ -24,46 +29,68 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-
 export const checkEmailIsAvailable = async (email) => {
-	const q = query(collection(db, "users"), where("email", "==", email));
-	const qsnap = await getDocs(q);
+  const q = query(collection(db, "users"), where("email", "==", email));
+  const qsnap = await getDocs(q);
 
-	return qsnap.empty;
+  return qsnap.empty;
 };
 
 export const checkEmailIsOnSystem = async (email) => {
-	const q = query(collection(db, "users"), where("email", "==", email));
-	const qsnap = await getDocs(q);
-  
-	return !qsnap.empty;
+  const q = query(collection(db, "users"), where("email", "==", email));
+  const qsnap = await getDocs(q);
+
+  return !qsnap.empty;
 };
 
 export function checkUsername(username) {
-	const q = query(collection(db, "users"), where("username", "==", username));
-	const qsnap = getDocs(q);
+  const q = query(collection(db, "users"), where("username", "==", username));
+  const qsnap = getDocs(q);
 
-	return qsnap.empty;
+  return qsnap.empty;
 }
 
-export const handleSignUp = ({ email, password, username, postcode }) => {
-	createUserWithEmailAndPassword(auth, email, password, postcode).then(
-		(userCredential) => {
-			setDoc(doc(db, "users", userCredential.user.uid), {
-				uid: userCredential.user.uid,
-				email: email,
-        username: username,
-        postcode: postcode,
-        avatar: '',
-        books: [],
-        wishlist: []
-			});
-		}
-	);
+const api = axios.create({
+  baseURL: "https://api.postcodes.io",
+});
+
+async function convertPostcode(postcode) {
+  try {
+    const response = await api.get(`/postcodes/${postcode}/`);
+    const result = {
+      longitude: response.data.result.longitude,
+      latitude: response.data.result.latitude,
+    };
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export const handleSignUp = async ({ email, password, username, postcode }) => {
+  const { longitude, latitude } = await convertPostcode(postcode);
+  return createUserWithEmailAndPassword(
+    auth,
+    email,
+    password,
+    longitude,
+    latitude
+  ).then((userCredential) => {
+    setDoc(doc(db, "users", userCredential.user.uid), {
+      uid: userCredential.user.uid,
+      email: email,
+      username: username,
+      longitude: longitude,
+      latitude: latitude,
+      avatar: "",
+      books: [],
+      wishlist: [],
+    });
+  });
 };
 
-export const handleLogin = ({password, email}) => {
-  signInWithEmailAndPassword(auth, email, password).then((userCredential)=>{
+export const handleLogin = ({ password, email }) => {
+  signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
     console.log(userCredential);
-  })
-}
+  });
+};
--- screens/LogInScreen.js
+++ screens/LogInScreen.js
@@ -1,12 +1,11 @@
 import * as React from "react";
 import { View, Text } from "react-native";
-import {LoginForm} from "../components/login-signup/Formik-Login"
+import { LoginForm } from "../components/login-signup/Formik-Login";
 
 export default function LogInScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-
-      <LoginForm/>
+      <LoginForm />
     </View>
   );
 }
