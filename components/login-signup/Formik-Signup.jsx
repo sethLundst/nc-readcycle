2368a7e15624542d03a0bfffae870c697d445c0c
apoteka1 <joseph.ackroyd.1991@gmail.com>
2022-02-24 16:56:27 +0000

    complete signup

--- components/login-signup/Formik-Signup.jsx
+++ components/login-signup/Formik-Signup.jsx
@@ -1,111 +1,128 @@
 //TODO : password confirm, validate email
 import axios from "axios";
-import React from "react";
+import React, { useContext } from "react";
 import { Button, TextInput, View, Text, StyleSheet } from "react-native";
 import { Formik } from "formik";
 import * as Yup from "yup";
 import { checkEmailIsAvailable, handleSignUp } from "../../db/firestore";
 import validator from "validator";
+import { UserContext } from "../../contexts/User";
 
 const pwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{7,20}$/;
 const pwordErrMsg =
-  "Password must be 7-20 characters long\nwith at least one uppercase letter,\none lowercase letter and one number.";
+	"Password must be 7-20 characters long\nwith at least one uppercase letter,\none lowercase letter and one number.";
 const postcodeRegex = /^[a-z]{1,2}\d[a-z\d]?\s*\d[a-z]{2}$/i;
-
 const api = axios.create({
-  baseURL: "https://api.postcodes.io",
+	baseURL: "https://api.postcodes.io",
 });
 
 async function validatePostcode(postcode) {
-  try {
-    const response = await api.get(`/postcodes/${postcode}/validate`);
-    return response.data.result;
-  } catch (err) {
-    console.log(err);
-  }
+	try {
+		const response = await api.get(`/postcodes/${postcode}/validate`);
+		return response.data.result;
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 const validationSchema = Yup.object({
-  email: Yup.string()
-    .ensure()
-    .required("E-mail required.")
-    .test("is valid", "invalid email", validator.isEmail)
-    .test("isAvailable", "Email in use", checkEmailIsAvailable),
-  password: Yup.string()
-    .ensure()
-    .required("Password required.")
-    .matches(pwordRegex, pwordErrMsg),
-  username: Yup.string()
-    .ensure()
-    .max(20, "Username must be 20 characters or under")
-    .required("Username required."),
-  postcode: Yup.string()
-    .ensure()
-    .matches(postcodeRegex, "Invalid post code.")
-    .test("Postcode exists", "Postcode does not exist", validatePostcode)
-    .required("Postcode required."),
+	email: Yup.string()
+		.ensure()
+		.required("E-mail required.")
+		.test("is valid", "invalid email", validator.isEmail)
+		.test("isAvailable", "Email in use", checkEmailIsAvailable),
+	password: Yup.string()
+		.ensure()
+		.required("Password required.")
+		.matches(pwordRegex, pwordErrMsg),
+	username: Yup.string()
+		.ensure()
+		.max(20, "Username must be 20 characters or under")
+		.required("Username required."),
+	postcode: Yup.string()
+		.ensure()
+		.matches(postcodeRegex, "Invalid post code.")
+		.test("Postcode exists", "Postcode does not exist", validatePostcode)
+		.required("Postcode required."),
 });
 
-export const SignupForm = ({ navigation }) => (
-  <Formik
-    initialValues={{ email: "", password: "", username: "", postcode: "" }}
-    onSubmit={(values) =>
-      handleSignUp(values).then(() => {
-        navigation.navigate("HomeScreen");
-      })
-    }
-    validationSchema={validationSchema}
-  >
-    {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
-      <View>
-        <TextInput
-          placeholder="email"
-          style={styles.textInput}
-          onChangeText={handleChange("email")}
-          onBlur={handleBlur("email")}
-          value={values.email}
-        />
-        {errors.email && touched.password && <Text>{errors.email}</Text>}
-        <TextInput
-          placeholder="password"
-          style={styles.textInput}
-          onChangeText={handleChange("password")}
-          onBlur={handleBlur("password")}
-          value={values.password}
-          secureTextEntry={true}
-        />
-        {errors.password && touched.password && <Text>{errors.password}</Text>}
-        <TextInput
-          placeholder="username"
-          style={styles.textInput}
-          onChangeText={handleChange("username")}
-          onBlur={handleBlur("username")}
-          value={values.username}
-        />
-        {errors.username && touched.username && <Text>{errors.username}</Text>}
-        <TextInput
-          placeholder="postcode"
-          style={styles.textInput}
-          onChangeText={handleChange("postcode")}
-          onBlur={handleBlur("postcode")}
-          value={values.postcode}
-        />
-        {errors.postcode && touched.postcode && <Text>{errors.postcode}</Text>}
-        <Button onPress={handleSubmit} title={"Submit"} />
-      </View>
-    )}
-  </Formik>
-);
+export const SignupForm = ({ navigation }) => {
+	const { user, setUser } = useContext(UserContext);
+
+	return (
+		<Formik
+			initialValues={{ email: "", password: "", username: "", postcode: "" }}
+			onSubmit={(values) =>
+				handleSignUp(values).then((newUser) => {
+					setUser(newUser);
+					return navigation.navigate("HomeScreen");
+				})
+			}
+			validationSchema={validationSchema}>
+			{({
+				handleChange,
+				handleBlur,
+				handleSubmit,
+				values,
+				errors,
+				touched,
+			}) => (
+				<View>
+					<TextInput
+						placeholder="email"
+						style={styles.textInput}
+						onChangeText={handleChange("email")}
+						onBlur={handleBlur("email")}
+						value={values.email}
+					/>
+					{errors.email && touched.password && <Text>{errors.email}</Text>}
+					<TextInput
+						placeholder="password"
+						style={styles.textInput}
+						onChangeText={handleChange("password")}
+						onBlur={handleBlur("password")}
+						value={values.password}
+						secureTextEntry={true}
+					/>
+					{errors.password && touched.password && (
+						<Text>{errors.password}</Text>
+					)}
+					<TextInput
+						placeholder="username"
+						style={styles.textInput}
+						onChangeText={handleChange("username")}
+						onBlur={handleBlur("username")}
+						value={values.username}
+					/>
+					{errors.username && touched.username && (
+						<Text>{errors.username}</Text>
+					)}
+					<TextInput
+						placeholder="postcode"
+						style={styles.textInput}
+						onChangeText={handleChange("postcode")}
+						onBlur={handleBlur("postcode")}
+						value={values.postcode}
+					/>
+					{errors.postcode && touched.postcode && (
+						<Text>{errors.postcode}</Text>
+					)}
+					<Button onPress={handleSubmit} title={"Submit"} />
+				</View>
+			)}
+		</Formik>
+	);
+};
 
 {
-  /* <TouchableOpacity style={styles.button} onPress={handleRegisterClick}>
+	/* <TouchableOpacity style={styles.button} onPress={handleRegisterClick}>
 <Text style={styles.buttonText}>Register.</Text>
 </TouchableOpacity> */
 }
 
 const styles = StyleSheet.create({
-  textInput: {
-    borderWidth: 1,
-    margin: 5,
-  },
+	textInput: {
+		borderWidth: 1,
+		margin: 5,
+	},
 });
--- db/firestore.js
+++ db/firestore.js
@@ -1,98 +1,109 @@
 import axios from "axios";
 import { initializeApp } from "firebase/app";
+import React, { useContext } from "react";
+import { UserContext } from "../contexts/User";
 import { getDoc, getFirestore } from "firebase/firestore";
 import {
-  collection,
-  query,
-  where,
-  getDocs,
-  addDoc,
-  setDoc,
-  doc,
+	collection,
+	query,
+	where,
+	getDocs,
+	addDoc,
+	setDoc,
+	doc,
 } from "firebase/firestore";
 import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
+	getAuth,
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAmmM3CBTdIeuOY7KEXouW-SFAHeyA_Ums",
-  authDomain: "readcycle-642e1.firebaseapp.com",
-  databaseURL: "https://readcycle-642e1-default-rtdb.firebaseio.com",
-  projectId: "readcycle-642e1",
-  storageBucket: "readcycle-642e1.appspot.com",
-  messagingSenderId: "465923556747",
-  appId: "1:465923556747:web:0238a1c3993ad960935b8e",
+	apiKey: "AIzaSyAmmM3CBTdIeuOY7KEXouW-SFAHeyA_Ums",
+	authDomain: "readcycle-642e1.firebaseapp.com",
+	databaseURL: "https://readcycle-642e1-default-rtdb.firebaseio.com",
+	projectId: "readcycle-642e1",
+	storageBucket: "readcycle-642e1.appspot.com",
+	messagingSenderId: "465923556747",
+	appId: "1:465923556747:web:0238a1c3993ad960935b8e",
 };
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
 export const checkEmailIsAvailable = async (email) => {
-  const q = query(collection(db, "users"), where("email", "==", email));
-  const qsnap = await getDocs(q);
+	const q = query(collection(db, "users"), where("email", "==", email));
+	const qsnap = await getDocs(q);
 
-  return qsnap.empty;
+	return qsnap.empty;
 };
 
 export const checkEmailIsOnSystem = async (email) => {
-  const q = query(collection(db, "users"), where("email", "==", email));
-  const qsnap = await getDocs(q);
+	const q = query(collection(db, "users"), where("email", "==", email));
+	const qsnap = await getDocs(q);
 
-  return !qsnap.empty;
+	return !qsnap.empty;
 };
 
 export function checkUsername(username) {
-  const q = query(collection(db, "users"), where("username", "==", username));
-  const qsnap = getDocs(q);
+	const q = query(collection(db, "users"), where("username", "==", username));
+	const qsnap = getDocs(q);
 
-  return qsnap.empty;
+	return qsnap.empty;
 }
 
 const api = axios.create({
-  baseURL: "https://api.postcodes.io",
+	baseURL: "https://api.postcodes.io",
 });
 
 async function convertPostcode(postcode) {
-  try {
-    const response = await api.get(`/postcodes/${postcode}/`);
-    const result = {
-      longitude: response.data.result.longitude,
-      latitude: response.data.result.latitude,
-    };
-    return result;
-  } catch (err) {
-    console.log(err);
-  }
+	try {
+		const response = await api.get(`/postcodes/${postcode}/`);
+		const result = {
+			longitude: response.data.result.longitude,
+			latitude: response.data.result.latitude,
+		};
+		return result;
+	} catch (err) {
+		console.log(err);
+	}
 }
 
 export const handleSignUp = async ({ email, password, username, postcode }) => {
-  const { longitude, latitude } = await convertPostcode(postcode);
-  return createUserWithEmailAndPassword(
-    auth,
-    email,
-    password,
-    longitude,
-    latitude
-  ).then((userCredential) => {
-    setDoc(doc(db, "users", userCredential.user.uid), {
-      uid: userCredential.user.uid,
-      email: email,
-      username: username,
-      longitude: longitude,
-      latitude: latitude,
-      avatar: "",
-      books: [],
-      wishlist: [],
-    });
-  });
+	let newUser = {};
+
+	try {
+		const { longitude, latitude } = await convertPostcode(postcode);
+
+		const userCredential = await createUserWithEmailAndPassword(
+			auth,
+			email,
+			password
+		);
+
+		newUser = {
+			uid: userCredential.user.uid,
+			email: email,
+			username: username,
+			longitude: longitude,
+			latitude: latitude,
+			avatar: "",
+			books: [],
+			wishlist: [],
+		};
+
+		await setDoc(doc(db, "users", userCredential.user.uid), newUser);
+
+		return newUser;
+	} catch (err) {
+		console.log(err);
+	}
 };
 
 export const handleLogin = ({ password, email }) => {
-  return signInWithEmailAndPassword(auth, email, password).then(
-    (userCredential) => {
-      console.log(userCredential);
-    }
-  );
+	return signInWithEmailAndPassword(auth, email, password).then(
+		(userCredential) => {
+			console.log(userCredential);
+		}
+	);
 };
