48d29cf5e933397096e3c174748bbbe26f7dadd6
Clayton <clayton.pyne7@gmail.com>
2022-02-24 14:51:30 +0000

    change variable names

--- screens/ListBookScreen.js
+++ screens/ListBookScreen.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import { View, Text } from "react-native";
 import ListBookForm from "../components/ListBookForm";
 
-export default function BarcodeScannerScreen({ navigation, route }) {
+export default function ListBookScreen({ navigation, route }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <ListBookForm navigation={navigation} route={route} />
