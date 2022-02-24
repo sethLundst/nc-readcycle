30b4983d0f56cc1babe5c5273a7c121e9d6714b9
Seth <seth@Seths-MacBook-Pro.local>
2022-02-24 12:18:06 +0000

    Add postcode validation

--- components/login-signup/Formik-Login.jsx
+++ components/login-signup/Formik-Login.jsx
@@ -6,47 +6,52 @@ import { handleLogin, checkEmailIsOnSystem } from "../../db/firestore";
 import validator from "validator";
 
 const validationSchema = Yup.object({
-	email: Yup.string()
-		.ensure()
-		.required("E-mail required.")
-		.test("is valid", "invalid email", validator.isEmail)
-		.test("isOnSystem", "Email not on system", checkEmailIsOnSystem),
-	password: Yup.string().ensure().required("Password required."),
+  email: Yup.string()
+    .ensure()
+    .required("E-mail required.")
+    .test("is valid", "invalid email", validator.isEmail)
+    .test("isOnSystem", "Email not on system", checkEmailIsOnSystem),
+  password: Yup.string().ensure().required("Password required."),
 });
 
-export const LoginForm = (props) => (
-	<Formik
-		initialValues={{ email: "", password: "" }}
-		onSubmit={handleLogin}
-		validationSchema={validationSchema}>
-		{({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
-			<View>
-				<TextInput
-					placeholder="email"
-					style={styles.textInput}
-					onChangeText={handleChange("email")}
-					onBlur={handleBlur("email")}
-					value={values.email}
-				/>
-				{errors.email && touched.password && <Text>{errors.email}</Text>}
-				<TextInput
-					placeholder="password"
-					style={styles.textInput}
-					onChangeText={handleChange("password")}
-					onBlur={handleBlur("password")}
-					value={values.password}
-					secureTextEntry
-				/>
-				{errors.password && touched.password && <Text>{errors.password}</Text>}
-				<Button onPress={handleSubmit} title="Log in." />
-			</View>
-		)}
-	</Formik>
+export const LoginForm = ({ navigation }) => (
+  <Formik
+    initialValues={{ email: "", password: "" }}
+    onSubmit={(values) =>
+      handleLogin(values).then(() => {
+        navigation.navigate("HomeScreen");
+      })
+    }
+    validationSchema={validationSchema}
+  >
+    {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
+      <View>
+        <TextInput
+          placeholder="email"
+          style={styles.textInput}
+          onChangeText={handleChange("email")}
+          onBlur={handleBlur("email")}
+          value={values.email}
+        />
+        {errors.email && touched.password && <Text>{errors.email}</Text>}
+        <TextInput
+          placeholder="password"
+          style={styles.textInput}
+          onChangeText={handleChange("password")}
+          onBlur={handleBlur("password")}
+          value={values.password}
+          secureTextEntry
+        />
+        {errors.password && touched.password && <Text>{errors.password}</Text>}
+        <Button onPress={handleSubmit} title="Log in." />
+      </View>
+    )}
+  </Formik>
 );
 
 const styles = StyleSheet.create({
-	textInput: {
-		borderWidth: 1,
-		margin: 5,
-	},
+  textInput: {
+    borderWidth: 1,
+    margin: 5,
+  },
 });
