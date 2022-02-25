1c8b08e9b6769a8f43b22d8f654e140678c4922d
apoteka1 <joseph.ackroyd.1991@gmail.com>
2022-02-25 11:55:11 +0000

    sort context

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { MainStackNavigator } from "./navigation/StackNavigators";
+import {UserProvider} from "./contexts/User"
 
 function App() {
+
   return (
+    <UserProvider>
     <NavigationContainer>
       <MainStackNavigator></MainStackNavigator>
     </NavigationContainer>
+    </UserProvider>
   );
 }
 export default App;
