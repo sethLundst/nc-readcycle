3ba3bb6d41a3028428599803f16614c9004de484
Seth <seth@Seths-MacBook-Pro.local>
2022-02-25 11:27:58 +0000

    Hide bottom tab navigation

---
+++ screens/SingleBookScreen.js
@@ -0,0 +1,6 @@
+import * as React from "react";
+import SingleBookPage from "../components/SingleBookPage";
+
+export default function SingleBookScreen() {
+  return <SingleBookPage></SingleBookPage>;
+}
