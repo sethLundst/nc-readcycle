5348eea24a7a1a267041b8ce977584e305b4edfa
apoteka1 <joseph.ackroyd.1991@gmail.com>
2022-02-26 18:59:16 +0000

    eh

---
+++ components/chat/Chat.js
@@ -0,0 +1,22 @@
+import React, {useContext} from "react";
+import { Button, TextInput, View, Text, StyleSheet } from "react-native";
+import { Formik } from "formik";
+import {getUserDetails, db, app, auth} from "../../db/firestore"
+import { UserContext } from "../../contexts/User";
+import { getDoc, getFirestore } from "firebase/firestore";
+
+
+
+
+// const Chat = (props) => {
+
+//   const createChat = (uid1, uid2) {
+
+//   }
+
+//   return ( 
+//     <Text>yo</Text>
+//    );
+// }
+ 
+// export default Chat;
