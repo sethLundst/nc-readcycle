457f7a83afd1599702b0b7ac927d194e44593f76
Seth <seth@Seths-MacBook-Pro.local>
2022-02-26 13:18:53 +0000

    Add default user

--- contexts/User.js
+++ contexts/User.js
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 export const UserContext = createContext();
 
 export function UserProvider({ children }) {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState("KQIFCO9Nv8O9Tji0SnTuVLCJ7hy2");
   return (
     <UserContext.Provider value={{ user, setUser }}>
       {children}
