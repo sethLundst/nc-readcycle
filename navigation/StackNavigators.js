c1bbca0e7b031fddfc613876c2ad81cb5a366b12
Seth <sethlundstram@protonmail.com>
2022-03-01 08:07:28 +0000

    Refactor

--- navigation/StackNavigators.js
+++ navigation/StackNavigators.js
@@ -1,92 +1,88 @@
-// TODO: Back button, no header, no tabs.
-
 import * as React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-const HomeStack = createNativeStackNavigator();
-import HomeScreen from "../screens/HomeScreen";
-
-const LogInStack = createNativeStackNavigator();
-import LogInScreen from "../screens/LogInScreen";
-import SignUpScreen from "../screens/SignUpScreen";
-
-const ListBookStack = createNativeStackNavigator();
-import ListBookScreen from "../screens/ListBookScreen";
-import ScannerScreen from "../screens/ScannerScreen";
-
-const MessagesStack = createNativeStackNavigator();
-import MessagesScreen from "../screens/MessagesScreen";
-import UserProfileScreen from "../screens/UserProfileScreen";
 import EditProfileScreen from "../screens/EditProfileScreen";
-import SingleBookScreen from "../screens/SingleBookScreen";
-
-const ProfileStack = createNativeStackNavigator();
-
-const SearchStack = createNativeStackNavigator();
-import SearchScreen from "../screens/SearchScreen";
-
-const LogOutStack = createNativeStackNavigator();
+import ListBookScreen from "../screens/ListBookScreen";
+import HomeScreen from "../screens/HomeScreen";
+import LogInScreen from "../screens/LogInScreen";
 import LogOutScreen from "../screens/LogOutScreen";
+import MessagesScreen from "../screens/MessagesScreen";
+import ScannerScreen from "../screens/ScannerScreen";
+import SearchScreen from "../screens/SearchScreen";
+import SignUpScreen from "../screens/SignUpScreen";
+import SingleBookScreen from "../screens/SingleBookScreen";
+import UserProfileScreen from "../screens/UserProfileScreen";
+
+const HomeStack = createNativeStackNavigator();
+const ListBookStack = createNativeStackNavigator();
+const LogInStack = createNativeStackNavigator();
+const LogOutStack = createNativeStackNavigator();
+const MessagesStack = createNativeStackNavigator();
+const ProfileStack = createNativeStackNavigator();
+const SearchStack = createNativeStackNavigator();
 
 export function HomeStackScreen() {
-	return (
-		<HomeStack.Navigator
-			initialRouteName="HomeScreen"
-			screenOptions={{ headerShown: false }}>
-			<HomeStack.Screen name="HomeScreen" component={HomeScreen} />
-			<HomeStack.Screen name="SingleBookScreen" component={SingleBookScreen} />
-		</HomeStack.Navigator>
-	);
+  return (
+    <HomeStack.Navigator
+      initialRouteName="HomeScreen"
+      screenOptions={{ headerShown: false }}
+    >
+      <HomeStack.Screen name="HomeScreen" component={HomeScreen} />
+      <HomeStack.Screen name="SingleBookScreen" component={SingleBookScreen} />
+    </HomeStack.Navigator>
+  );
 }
 
 export function LogInStackScreen() {
-	return (
-		<LogInStack.Navigator
-			initialRouteName="SignUpScreen"
-			screenOptions={{ headerShown: false }}>
-			<LogInStack.Screen
-				name="SignUpScreen"
-				component={SignUpScreen}
-				options={{ headerShown: false }}
-			/>
-			<LogInStack.Screen name="LogInScreen" component={LogInScreen} />
-			<LogInStack.Screen name="HomeScreen" component={HomeScreen} />
-			<LogInStack.Screen name="SingleBookScreen" component={SingleBookScreen} />
-		</LogInStack.Navigator>
-	);
+  return (
+    <LogInStack.Navigator
+      initialRouteName="SignUpScreen"
+      screenOptions={{ headerShown: false }}
+    >
+      <LogInStack.Screen
+        name="SignUpScreen"
+        component={SignUpScreen}
+        options={{ headerShown: false }}
+      />
+      <LogInStack.Screen name="LogInScreen" component={LogInScreen} />
+      <LogInStack.Screen name="HomeScreen" component={HomeScreen} />
+      <LogInStack.Screen name="SingleBookScreen" component={SingleBookScreen} />
+    </LogInStack.Navigator>
+  );
 }
 
 export function ListBookStackScreen() {
-	return (
-		<ListBookStack.Navigator
-			initialRouteName="ListBookScreen"
-			screenOptions={{ headerShown: false }}>
-			<ListBookStack.Screen name="ListBookScreen" component={ListBookScreen} />
-			<ListBookStack.Screen name="ScannerScreen" component={ScannerScreen} />
-		</ListBookStack.Navigator>
-	);
+  return (
+    <ListBookStack.Navigator
+      initialRouteName="ListBookScreen"
+      screenOptions={{ headerShown: false }}
+    >
+      <ListBookStack.Screen name="ListBookScreen" component={ListBookScreen} />
+      <ListBookStack.Screen name="ScannerScreen" component={ScannerScreen} />
+    </ListBookStack.Navigator>
+  );
 }
 
 export function MessagesStackScreen() {
-	return (
-		<MessagesStack.Navigator
-			initialRouteName="MessagesScreen"
-			screenOptions={{ headerShown: false }}>
-			<MessagesStack.Screen name="MessagesScreen" component={MessagesScreen} />
-			<MessagesStack.Screen
-				name="SingleBookScreen"
-				component={SingleBookScreen}
-			/>
-			<MessagesStack.Screen
-				name="UserProfileScreen"
-				component={UserProfileScreen}
-			/>
-		</MessagesStack.Navigator>
-	);
+  return (
+    <MessagesStack.Navigator
+      initialRouteName="MessagesScreen"
+      screenOptions={{ headerShown: false }}
+    >
+      <MessagesStack.Screen name="MessagesScreen" component={MessagesScreen} />
+      <MessagesStack.Screen
+        name="SingleBookScreen"
+        component={SingleBookScreen}
+      />
+      <MessagesStack.Screen
+        name="UserProfileScreen"
+        component={UserProfileScreen}
+      />
+    </MessagesStack.Navigator>
+  );
 }
 
 export function ProfileStackScreen() {
-
   return (
     <ProfileStack.Navigator
       initialRouteName="UserProfileScreen"
@@ -106,39 +102,20 @@ export function ProfileStackScreen() {
       />
     </ProfileStack.Navigator>
   );
-
-}
-
-export function SearchStackScreen() {
-	return (
-		<SearchStack.Navigator
-			initialRouteName="SearchScreen"
-			screenOptions={{ headerShown: false }}>
-			<SearchStack.Screen name="SearchScreen" component={SearchScreen} />
-			<SearchStack.Screen
-				name="UserProfileScreen"
-				component={UserProfileScreen}
-			/>
-			<SearchStack.Screen
-				name="SingleBookScreen"
-				component={SingleBookScreen}
-			/>
-		</SearchStack.Navigator>
-	);
 }
 
 export function LogOutStackScreen() {
-	return (
-		<LogOutStack.Navigator
-			initialRouteName="LogOutScreen"
-      
-			screenOptions={{ headerShown: false }}>
-			<LogOutStack.Screen name="LogOutScreen" component={LogOutScreen} />
-			<LogInStack.Screen
-				name="SignUpScreen"
-				component={SignUpScreen}
-				options={{ headerShown: false }}
-			/>
-		</LogOutStack.Navigator>
-	);
+  return (
+    <LogOutStack.Navigator
+      initialRouteName="LogOutScreen"
+      screenOptions={{ headerShown: false }}
+    >
+      <LogOutStack.Screen name="LogOutScreen" component={LogOutScreen} />
+      <LogInStack.Screen
+        name="SignUpScreen"
+        component={SignUpScreen}
+        options={{ headerShown: false }}
+      />
+    </LogOutStack.Navigator>
+  );
 }
--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -3,74 +3,68 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
 import {
-	HomeStackScreen,
-	ListBookStackScreen,
-	MessagesStackScreen,
-	ProfileStackScreen,
-	SearchStackScreen,
-  LogOutStackScreen
+  HomeStackScreen,
+  ListBookStackScreen,
+  MessagesStackScreen,
+  ProfileStackScreen,
+  SearchStackScreen,
+  LogOutStackScreen,
 } from "../navigation/StackNavigators";
 
 const Tab = createBottomTabNavigator();
 
 function BottomTabNavigator() {
-	return (
-		<Tab.Navigator
-			initialRouteName="HomeStackScreen"
-			screenOptions={({ route }) => ({
-				tabBarIcon: ({ color, size }) => {
-					let iconName;
-					if (route.name === "HomeStackScreen") {
-						iconName = "home-outline";
-					} else if (route.name === "ListBookStackScreen") {
-						iconName = "barcode-outline";
-					} else if (route.name === "SearchStackScreen") {
-						iconName = "search-outline";
-					} else if (route.name === "MessagesStackScreen") {
-						iconName = "chatbubbles-outline";
-					} else if (route.name === "ProfileStackScreen") {
-						iconName = "person-outline";
-					} else if (route.name === "LogOutStackScreen") {
-            iconName = "log-out-outline"
+  return (
+    <Tab.Navigator
+      initialRouteName="HomeStackScreen"
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+          if (route.name === "HomeStackScreen") {
+            iconName = "home-outline";
+          } else if (route.name === "ListBookStackScreen") {
+            iconName = "barcode-outline";
+          } else if (route.name === "MessagesStackScreen") {
+            iconName = "chatbubbles-outline";
+          } else if (route.name === "ProfileStackScreen") {
+            iconName = "person-outline";
+          } else if (route.name === "LogOutStackScreen") {
+            iconName = "log-out-outline";
           }
-					return <Ionicons name={iconName} size={size} color={color} />;
-				},
-				tabBarActiveTintColor: "green",
-				tabBarInactiveTintColor: "gray",
-				tabBarLabel: "",
-			})}>
-			<Tab.Screen
-				name="HomeStackScreen"
-				component={HomeStackScreen}
-				options={{ title: "Home", showLabel: false }}
-			/>
-			<Tab.Screen
-				name="ListBookStackScreen"
-				component={ListBookStackScreen}
-				options={{ title: "List Book" }}
-			/>
-			<Tab.Screen
-				name="SearchStackScreen"
-				component={SearchStackScreen}
-				options={{ title: "Search" }}
-			/>
-			<Tab.Screen
-				name="MessagesStackScreen"
-				component={MessagesStackScreen}
-				options={{ title: "Messages" }}
-			/>
-			<Tab.Screen
-				name="ProfileStackScreen"
-				component={ProfileStackScreen}
-				options={{ title: "Profile" }}
-			/>
-			<Tab.Screen
-				name="LogOutStackScreen"
-				component={LogOutStackScreen}
-				options={{ title: "Log Out" }}
-			/>
-		</Tab.Navigator>
-	);
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "green",
+        tabBarInactiveTintColor: "gray",
+        tabBarLabel: "",
+      })}
+    >
+      <Tab.Screen
+        name="HomeStackScreen"
+        component={HomeStackScreen}
+        options={{ title: "Home", showLabel: false }}
+      />
+      <Tab.Screen
+        name="ListBookStackScreen"
+        component={ListBookStackScreen}
+        options={{ title: "List Book" }}
+      />
+      <Tab.Screen
+        name="MessagesStackScreen"
+        component={MessagesStackScreen}
+        options={{ title: "Messages" }}
+      />
+      <Tab.Screen
+        name="ProfileStackScreen"
+        component={ProfileStackScreen}
+        options={{ title: "Profile" }}
+      />
+      <Tab.Screen
+        name="LogOutStackScreen"
+        component={LogOutStackScreen}
+        options={{ title: "Log Out" }}
+      />
+    </Tab.Navigator>
+  );
 }
 
 export default BottomTabNavigator;
