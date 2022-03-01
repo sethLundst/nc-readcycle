2a6fd15e14eabb8f40a8c50ed893c19b18324a3d
apoteka1 <joseph.ackroyd.1991@gmail.com>
2022-03-01 22:38:08 +0000

    add send message and single message screen

--- db/firestore.js
+++ db/firestore.js
@@ -4,210 +4,216 @@ import React, { useContext } from "react";
 import { UserContext } from "../contexts/User";
 import { getDoc, getFirestore } from "firebase/firestore";
 import { getDistance, convertDistance } from "geolib";
+const timestamp = require('time-stamp');
 import {
-  collection,
-  query,
-  where,
-  getDocs,
-  addDoc,
-  setDoc,
-  doc,
-  arrayUnion,
-  updateDoc,
+	collection,
+	query,
+	where,
+	getDocs,
+	addDoc,
+	setDoc,
+	doc,
+	arrayUnion,
+	updateDoc,
 } from "firebase/firestore";
 import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  setPersistence,
-  browserLocalPersistence,
-  onAuthStateChanged,
+	getAuth,
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+	setPersistence,
+	browserLocalPersistence,
+	onAuthStateChanged,
 } from "firebase/auth";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAmmM3CBTdIeuOY7KEXouW-SFAHeyA_Ums",
-  authDomain: "readcycle-642e1.firebaseapp.com",
-  databaseURL: "https://readcycle-642e1-default-rtdb.firebaseio.com",
-  projectId: "readcycle-642e1",
-  storageBucket: "readcycle-642e1.appspot.com",
-  messagingSenderId: "465923556747",
-  appId: "1:465923556747:web:0238a1c3993ad960935b8e",
+	apiKey: "AIzaSyAmmM3CBTdIeuOY7KEXouW-SFAHeyA_Ums",
+	authDomain: "readcycle-642e1.firebaseapp.com",
+	databaseURL: "https://readcycle-642e1-default-rtdb.firebaseio.com",
+	projectId: "readcycle-642e1",
+	storageBucket: "readcycle-642e1.appspot.com",
+	messagingSenderId: "465923556747",
+	appId: "1:465923556747:web:0238a1c3993ad960935b8e",
 };
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 
 export const checkEmailIsAvailable = async (email) => {
-  const q = query(collection(db, "users"), where("email", "==", email));
-  const qsnap = await getDocs(q);
+	const q = query(collection(db, "users"), where("email", "==", email));
+	const qsnap = await getDocs(q);
 
-  return qsnap.empty;
+	return qsnap.empty;
 };
 
 export const checkEmailIsOnSystem = async (email) => {
-  const q = query(collection(db, "users"), where("email", "==", email));
-  const qsnap = await getDocs(q);
+	const q = query(collection(db, "users"), where("email", "==", email));
+	const qsnap = await getDocs(q);
 
-  return !qsnap.empty;
+	return !qsnap.empty;
 };
 
 export const checkUsername = async (username) => {
-  const q = query(collection(db, "users"), where("username", "==", username));
-  const qsnap = await getDocs(q);
+	const q = query(collection(db, "users"), where("username", "==", username));
+	const qsnap = await getDocs(q);
 
-  return qsnap.empty;
+	return qsnap.empty;
 };
 
 const api = axios.create({
-  baseURL: "https://api.postcodes.io",
+	baseURL: "https://api.postcodes.io",
 });
 
 async function convertPostcode(postcode) {
-  try {
-    const response = await api.get(`/postcodes/${postcode}/`);
-    console.log(response.data.european_electoral_region);
-    const result = {
-      longitude: response.data.result.longitude,
-      latitude: response.data.result.latitude,
-      region: response.data.result.european_electoral_region,
-    };
-    return result;
-  } catch (err) {
-    console.log(err);
-  }
+	try {
+		const response = await api.get(`/postcodes/${postcode}/`);
+		console.log(response.data.european_electoral_region);
+		const result = {
+			longitude: response.data.result.longitude,
+			latitude: response.data.result.latitude,
+			region: response.data.result.european_electoral_region,
+		};
+		return result;
+	} catch (err) {
+		console.log(err);
+	}
 }
 
 export const handleSignUp = async ({
-  email,
-  password,
-  username,
-  postcode,
-  city,
+	email,
+	password,
+	username,
+	postcode,
+	city,
 }) => {
-  let newUser = {};
+	let newUser = {};
 
-  try {
-    await setPersistence(auth, browserLocalPersistence);
+	try {
+		await setPersistence(auth, browserLocalPersistence);
 
-    const { longitude, latitude, region } = await convertPostcode(
-      postcode
-    ).catch();
+		const { longitude, latitude, region } = await convertPostcode(
+			postcode
+		).catch();
 
-    const userCredential = await createUserWithEmailAndPassword(
-      auth,
-      email,
-      password
-    );
+		const userCredential = await createUserWithEmailAndPassword(
+			auth,
+			email,
+			password
+		);
 
-    newUser = {
-      uid: userCredential.user.uid,
-      email: email,
-      username: username,
-      avatar_url: "", //placeholder?
-      books: [],
-      chats: [],
-      lent: 0,
-      coordinates: { latitude: latitude, longitude: longitude },
-      region: region,
-      city: city,
-    };
+		newUser = {
+			uid: userCredential.user.uid,
+			email: email,
+			username: username,
+			avatar_url: "", //placeholder?
+			books: [],
+			chats: [],
+			lent: 0,
+			coordinates: { latitude: latitude, longitude: longitude },
+			region: region,
+			city: city,
+		};
 
-    await setDoc(doc(db, "users", userCredential.user.uid), newUser);
-    console.log("done");
-    return newUser.uid;
-  } catch (err) {
-    console.log(err);
-  }
+		await setDoc(doc(db, "users", userCredential.user.uid), newUser);
+		console.log("done");
+		return newUser.uid;
+	} catch (err) {
+		console.log(err);
+	}
 };
 
 export const handleLogin = async (password, email) => {
-  return signInWithEmailAndPassword(auth, email, password).then(
-    (userCredential) => {
-      return userCredential.user.uid;
-    }
-  );
+	return signInWithEmailAndPassword(auth, email, password).then(
+		(userCredential) => {
+			return userCredential.user.uid;
+		}
+	);
 };
 
 export const sendBook = async (bookObject, user) => {
-  const docRef = doc(db, "users", user);
-  await updateDoc(docRef, {
-    books: arrayUnion(bookObject),
-  });
+	const docRef = doc(db, "users", user);
+	await updateDoc(docRef, {
+		books: arrayUnion(bookObject),
+	});
 };
 
 export const getUsersByLocation = async (region) => {
-  const q = query(collection(db, "users"), where("location", "==", region));
+	const q = query(collection(db, "users"), where("location", "==", region));
 };
 
 export const getUserDetails = async (uid) => {
-  const docRef = doc(db, "users", `${uid}`);
-  const docSnap = await getDoc(docRef);
-  return docSnap.data();
+	const docRef = doc(db, "users", `${uid}`);
+	const docSnap = await getDoc(docRef);
+	return docSnap.data();
 };
 
 export const deleteBook = async (book, isLent) => {};
 
 export const createChat = async (members, book) => {
-  const chatKey = `${members[0]}${members[1]}${book.id}`;
-  const q = query(
-    collection(db, "users"),
-    where("chats", "array-contains", chatKey)
-  );
-  const qsnap = await getDocs(q);
+	const chatKey = `${members[0]}${members[1]}${book.id}`;
+	const q = query(
+		collection(db, "users"),
+		where("chats", "array-contains", chatKey)
+	);
+	const qsnap = await getDocs(q);
 
-  if (qsnap.empty) {
-    const docRef = doc(db, "users", members[0]);
-    await updateDoc(docRef, { chats: arrayUnion(chatKey) });
-    await setDoc(doc(db, "chats", chatKey), {
-      members: members,
-      book: book.title,
-      book_id: book.id,
-      // picture: book.altImage,
-      messages: [],
-      id: chatKey,
-    });
-  }
-  return chatKey;
+	if (qsnap.empty) {
+		const docRef = doc(db, "users", members[0]);
+		await updateDoc(docRef, { chats: arrayUnion(chatKey) });
+		await setDoc(doc(db, "chats", chatKey), {
+			members: members,
+			book: book.title,
+			book_id: book.id,
+			// picture: book.altImage,
+			messages: [],
+			id: chatKey,
+		});
+	}
+	return chatKey;
 };
 
 export const getAllUsers = async () => {
-  let result = [];
+	let result = [];
 
-  const querySnapshot = await getDocs(collection(db, "users"));
-  querySnapshot.forEach((doc) => {
-    result.push(doc.data());
-  });
+	const querySnapshot = await getDocs(collection(db, "users"));
+	querySnapshot.forEach((doc) => {
+		result.push(doc.data());
+	});
 
-  return result;
+	return result;
 };
 
 export const uploadProfilePic = async (uri) => {
-  const storage = getStorage();
-  const picRef = ref(storage, "profilepic.jpg");
-  const img = await fetch(uri);
-  const bytes = await img.blob();
+	const storage = getStorage();
+	const picRef = ref(storage, "profilepic.jpg");
+	const img = await fetch(uri);
+	const bytes = await img.blob();
 
-  await uploadBytes(picRef, bytes);
+	await uploadBytes(picRef, bytes);
 
-  const profilePicture = await getDownloadURL(picRef);
+	const profilePicture = await getDownloadURL(picRef);
 
-  await updateDoc(doc(db, "users", auth.currentUser.uid), {
-    avatar_url: profilePicture,
-  });
+	await updateDoc(doc(db, "users", auth.currentUser.uid), {
+		avatar_url: profilePicture,
+	});
 };
 
 export const getChat = async (chatID) => {
-  const docRef = doc(db, "chats", `${chatID}`);
-  const docSnap = await getDoc(docRef);
-  return docSnap.data();
+	const docRef = doc(db, "chats", `${chatID}`);
+	const docSnap = await getDoc(docRef);
+	return docSnap.data();
 };
 
 export const addMessage = async (chatID, username, data) => {
-  const messageObject = {
-    username: username,
-    message: data,
-  };
-  const docRef = doc(db, "chats", chatID);
-  await updateDoc(docRef, { messages: arrayUnion(messageObject) });
+
+	const messageObject = {
+		username: username,
+		message: data,
+    postedAt: timestamp("DD/MM/YYYY:HH:mm:ss:ms")
+    
+	};
+	const docRef = doc(db, "chats", `${chatID}`);
+	await updateDoc(docRef, {
+		messages: arrayUnion(messageObject),
+	});
 };
--- screens/SingleMessageScreen.js
+++ screens/SingleMessageScreen.js
@@ -1,76 +1,134 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Formik } from "formik";
 import { addMessage, getChat, getUserDetails } from "../db/firestore";
-import { getDoc, getFirestore } from "firebase/firestore";
+import { getDoc, getFirestore, Timestamp } from "firebase/firestore";
 import { UserContext } from "../contexts/User";
+import { SafeAreaView } from "react-native-safe-area-context";
+const timestamp = require('time-stamp');
 import {
-  Button,
-  TextInput,
-  View,
-  Text,
-  StyleSheet,
-  Pressable,
-  useWindowDimensions,
+	Button,
+	TextInput,
+	View,
+	Text,
+	StyleSheet,
+	Pressable,
+	useWindowDimensions,
+	FlatList,
 } from "react-native";
 
+
+
 export default function SingleMessageScreen({ route, navigation }) {
-  const { user } = useContext(UserContext);
-  const { chatID } = route.params;
-  const [username, setUsername] = useState("");
-  const [newMessage, setNewMessage] = useState("");
+	const { user } = useContext(UserContext);
+	const { chatID } = route.params;
+	const [currUser, setCurrUser] = useState("");
+	const [otherUser, setOtherUser] = useState("");
+	const [newMessage, setNewMessage] = useState("");
+	const [messages, setMessages] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [title, setTitle] = useState("");
+	console.log(otherUser);
 
-  function handleChange(value) {
-    setNewMessage(value);
-  }
+  const Comment = (props) => {
+    const { item } = props;
+    const time = item.postedAt.slice(0,10)+' '+item.postedAt.slice(11,16)
+    return (
+      <View>
+        <Text>{item.username} {time}</Text>
+        <Text>{item.message}</Text>
+      </View>
+    );
+  };
 
-  function handleSubmit() {
-    addMessage(chatID, username, newMessage);
-    setNewMessage("");
-  }
+	function handleChange(value) {
+		setNewMessage(value);
+	}
 
-  useEffect(async () => {
-    const chat = await getChat(chatID);
-    const { username } = await getUserDetails(user);
-    setUsername(username);
-  }, []);
+	function handleSubmit() {
+		addMessage(chatID, currUser.username, newMessage).then(() => {
+			setMessages((curr) => [
+				...curr,
+				{ username: currUser.username, message: newMessage, postedAt: timestamp("DD/MM/YYYY:HH:mm:ss:ms")}
+			]);
+			setNewMessage("");
+		});
+	}
 
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Single msg screen</Text>
-      <TextInput
-        placeholder="New message"
-        style={styles.textInput}
-        onChangeText={handleChange}
-        value={newMessage}
-      />
-      <Pressable style={styles.submit} onPress={handleSubmit}>
-        <Text>Send</Text>
-      </Pressable>
-    </View>
-  );
+	useEffect(async () => {
+		setIsLoading(true);
+		const chat = await getChat(chatID);
+		const currUser = await getUserDetails(user);
+		setCurrUser(currUser);
+		const msgs = [];
+    console.log(chat);
+		if (chat.messages.length > 0) {
+			chat.messages.forEach((message) => {
+				msgs.push(message);
+			});
+			setMessages(msgs);
+		}
+		for (const user of chat.members) {
+			if (user !== currUser) {
+				const otherUserObj = await getUserDetails(user);
+
+				setOtherUser(otherUserObj);
+			}
+		}
+
+		setTitle(chat.book);
+
+		setIsLoading(false);
+	}, []);
+
+	return isLoading ? (
+		<Text>Loading...</Text>
+	) : (
+		<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+			<Text>
+				{title} || {otherUser.username}
+			</Text>
+			<SafeAreaView style={styles.container}>
+				<FlatList
+					data={messages}
+					renderItem={Comment}
+					keyExtractor={(item, index) => item.postedAt+index}
+				/>
+			</SafeAreaView>
+
+			<TextInput
+				placeholder="New message"
+				style={styles.textInput}
+				onChangeText={handleChange}
+				value={newMessage}
+			/>
+			<Pressable style={styles.submit} onPress={handleSubmit}>
+				<Text>Send</Text>
+			</Pressable>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  submit: {
-    borderWidth: 1,
-    marginLeft: 5,
-    paddingLeft: 5,
-    width: 55,
-    height: 55,
-    backgroundColor: "white",
-    margin: 10,
-    padding: 8,
-    borderRadius: 14,
-  },
-  textInput: {
-    borderWidth: 1,
-    marginLeft: 5,
-    paddingLeft: 5,
-    width: 300,
-    height: 55,
-    backgroundColor: "white",
-    margin: 10,
-    padding: 8,
-    borderRadius: 14,
-  },
+	submit: {
+		borderWidth: 1,
+		marginLeft: 5,
+		paddingLeft: 5,
+		width: 55,
+		height: 55,
+		backgroundColor: "white",
+		margin: 10,
+		padding: 8,
+		borderRadius: 14,
+	},
+	textInput: {
+		borderWidth: 1,
+		marginLeft: 5,
+		paddingLeft: 5,
+		width: 300,
+		height: 55,
+		backgroundColor: "white",
+		margin: 10,
+		padding: 8,
+		borderRadius: 14,
+	},
 });
