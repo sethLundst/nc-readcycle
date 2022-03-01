92b0bd1c06ce9e29cb0feafe2fa31c964ef01a92
Seth <seth@Seths-MacBook-Pro.local>
2022-03-01 16:44:04 +0000

    Add chat functionality

--- screens/SingleMessageScreen.js
+++ screens/SingleMessageScreen.js
@@ -1,25 +1,76 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Formik } from "formik";
-import { getChat } from "../db/firestore";
-import { UserContext } from "../contexts/User";
+import { addMessage, getChat, getUserDetails } from "../db/firestore";
 import { getDoc, getFirestore } from "firebase/firestore";
+import { UserContext } from "../contexts/User";
 import {
-	Button,
-	TextInput,
-	View,
-	Text,
-	StyleSheet,
-	useWindowDimensions,
+  Button,
+  TextInput,
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  useWindowDimensions,
 } from "react-native";
 
 export default function SingleMessageScreen({ route, navigation }) {
-	const { chatID } = route.params;
-	
-  
+  const { user } = useContext(UserContext);
+  const { chatID } = route.params;
+  const [username, setUsername] = useState("");
+  const [newMessage, setNewMessage] = useState("");
 
-	return (
-		<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-			<Text>Single msg screen</Text>
-		</View>
-	);
+  function handleChange(value) {
+    setNewMessage(value);
+  }
+
+  function handleSubmit() {
+    addMessage(chatID, username, newMessage);
+    setNewMessage("");
+  }
+
+  useEffect(async () => {
+    const chat = await getChat(chatID);
+    const { username } = await getUserDetails(user);
+    setUsername(username);
+  }, []);
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Single msg screen</Text>
+      <TextInput
+        placeholder="New message"
+        style={styles.textInput}
+        onChangeText={handleChange}
+        value={newMessage}
+      />
+      <Pressable style={styles.submit} onPress={handleSubmit}>
+        <Text>Send</Text>
+      </Pressable>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  submit: {
+    borderWidth: 1,
+    marginLeft: 5,
+    paddingLeft: 5,
+    width: 55,
+    height: 55,
+    backgroundColor: "white",
+    margin: 10,
+    padding: 8,
+    borderRadius: 14,
+  },
+  textInput: {
+    borderWidth: 1,
+    marginLeft: 5,
+    paddingLeft: 5,
+    width: 300,
+    height: 55,
+    backgroundColor: "white",
+    margin: 10,
+    padding: 8,
+    borderRadius: 14,
+  },
+});
