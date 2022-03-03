67d83e7a18da111cdcf94ac1eca29f060721ed79
James_G <jamesg.chef@gmail.com>
2022-03-03 18:05:54 +0000

    add layout changes to single message page and home page, not finished

--- screens/SingleMessageScreen.js
+++ screens/SingleMessageScreen.js
@@ -35,7 +35,7 @@ export default function SingleMessageScreen({ route, navigation }) {
     return (
       <View style={styles.commentBox}>
         <View style={styles.comment}>
-          <Text>
+          <Text styles={styles.commentSnippet}>
             {item.username} {time}
           </Text>
           <Text>{item.message}</Text>
@@ -92,42 +92,42 @@ export default function SingleMessageScreen({ route, navigation }) {
     <Text>Loading...</Text>
   ) : (
     <SafeAreaView style={styles.container}>
-      <LinearGradient
-        // Background Linear Gradient
-        colors={["#f7edf2", "#dee2ff", "white"]}
-        start={{
-          x: 0,
-          y: 0,
-        }}
-        end={{
-          x: 1,
-          y: 1,
-        }}
-        style={styles.background}
-      />
-      <View style={styles.header}>
-        <View style={styles.imageBackground}>
-          <Image
-            style={styles.pic}
-            source={{
-              uri: image,
-            }}
+      <KeyboardAvoidingView behavior="padding">
+        <LinearGradient
+          // Background Linear Gradient
+          colors={["#f7edf2", "#dee2ff", "white"]}
+          start={{
+            x: 0,
+            y: 0,
+          }}
+          end={{
+            x: 1,
+            y: 1,
+          }}
+          style={styles.background}
+        />
+        <View style={styles.header}>
+          <View style={styles.imageBackground}>
+            <Image
+              style={styles.pic}
+              source={{
+                uri: image,
+              }}
+            />
+          </View>
+          <Text>
+            {title} || {otherUser.username}
+          </Text>
+        </View>
+        <View style={styles.list}>
+          <FlatList
+            data={messages}
+            renderItem={Comment}
+            keyExtractor={(item, index) => item.postedAt + index}
           />
         </View>
-        <Text>
-          {title} || {otherUser.username}
-        </Text>
-      </View>
-      <View style={styles.list}>
-        <FlatList
-          data={messages}
-          renderItem={Comment}
-          keyExtractor={(item, index) => item.postedAt + index}
-        />
-      </View>
 
-      <View style={styles.bottomContainer}>
-        <KeyboardAvoidingView behavior="padding">
+        <View style={styles.bottomContainer}>
           <TextInput
             placeholder="New message"
             style={styles.textInput}
@@ -135,12 +135,12 @@ export default function SingleMessageScreen({ route, navigation }) {
             value={newMessage}
             multiline={true}
           />
-        </KeyboardAvoidingView>
-        <Pressable style={styles.submit} onPress={handleSubmit}>
-          <Text>Send</Text>
-        </Pressable>
-      </View>
 
+          <Pressable style={styles.submit} onPress={handleSubmit}>
+            <Text>Send</Text>
+          </Pressable>
+        </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
@@ -148,9 +148,9 @@ export default function SingleMessageScreen({ route, navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    paddingTop: 20,
+    //alignItems: "center",
+    //justifyContent: "flex-end",
+    paddingTop: 0,
     // backgroundColor: "white",
   },
   background: {
@@ -158,10 +158,22 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     top: 0,
-    height: "120%",
+	  height: "100%",
+	width: "100%"
+  },
+  header: {
+    height: 200,
+    //flex: 1,
+    alignItems: "center",
+    // justifyContent: "space-between",
+    margin: 0,
+    // borderColor: "red",
+    // borderWidt: 2,
   },
   imageBackground: {
-    marginTop: 150,
+    borderColor: "red",
+    borderWidth: 6,
+    marginTop: 50,
     height: 104,
     width: 104,
     borderRadius: 100,
@@ -181,6 +193,75 @@ const styles = StyleSheet.create({
     height: 100,
     width: 100,
     borderRadius: 100,
+  },
+  list: {
+    height: "40%",
+    margin: 10,
+    marginVertical: 20,
+    // borderColor: "red",
+    // borderWidth: 6,
+  },
+	commentBox: {
+	  alignSelf: "center",
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+	  shadowColor: "black",
+	width: 300,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 5,
+    shadowRadius: 4,
+    elevation: 3,
+    // borderColor: "red",
+    // borderWidth: 2,
+  },
+  comment: {
+    margin: 0,
+    backgroundColor: "#ffbd03",
+    borderColor: "white",
+    borderWidth: 3,
+    // borderColor: "red",
+    // borderWidth: 2,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 6,
+	},
+	commentSnippet: {
+		fontFamily: "HelveticaNeue",
+		fontSize: 16,
+	},
+	bottomContainer: {
+	flex: 1,
+    //height: 100,
+    alignItems: "center",
+    justifyContent: "flex-end",
+    marginBottom: 0,
+    // borderColor: "red",
+    // borderWidth: 2,
+  },
+  textInput: {
+    borderWidth: 2,
+    width: 300,
+    height: 100,
+    marginTop: 0,
+    backgroundColor: "white",
+    margin: 10,
+    padding: 10,
+    borderRadius: 10,
+    borderColor: "grey",
+    shadowColor: "white",
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 4,
+    shadowRadius: 17,
+
+    elevation: 10,
+    // borderColor: "red",
+    // borderWidth: 6,
   },
   submit: {
     borderRadius: 25,
@@ -198,95 +279,13 @@ const styles = StyleSheet.create({
 
     alignItems: "center",
     justifyContent: "center",
-	},
-	sendText: {
-		textAlign: "center",
-		fontSize: 16,
-		fontFamily: "HelveticaNeue",
-		color: "white",
-		padding: 6,
   },
-  bottomContainer: {
-    height: 80,
-    alignItems: "center",
-    justifyContent: "center",
-    marginBottom: 80,
+  sendText: {
+    textAlign: "center",
+    fontSize: 16,
+    fontFamily: "HelveticaNeue",
+    color: "white",
+    padding: 6,
   },
-  textInput: {
-    borderWidth: 2,
-    width: 300,
-    height: 80,
-    marginTop: 100,
-    backgroundColor: "white",
-    margin: 10,
-    padding: 10,
-    borderRadius: 10,
-    borderColor: "grey",
-    shadowColor: "white",
-    shadowOffset: {
-      width: 0,
-      height: 0,
-    },
-    shadowOpacity: 4,
-    shadowRadius: 17,
-
-    elevation: 10,
-  },
-  header: {
-    height: "40%",
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "space-between",
-    margin: 60,
-  },
-  list: {
-    height: "55%",
-    margin: 10,
-    marginTop: 60,
-  },
-  commentBox: {
-    paddingHorizontal: 10,
-    paddingVertical: 10,
-    shadowColor: "black",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 5,
-    shadowRadius: 4,
-    elevation: 3,
-  },
-  comment: {
-    margin: 0,
-    backgroundColor: "#ffbd03",
-    borderColor: "white",
-    borderWidth: 3,
-    // borderColor: "red",
-    // borderWidth: 2,
-    borderRadius: 10,
-    paddingHorizontal: 10,
-    paddingVertical: 8,
-  },
-  imageBackground: {
-    marginTop: 0,
-    height: 104,
-    width: 104,
-    borderRadius: 100,
-    shadowColor: "grey",
-    shadowOffset: {
-      width: 0,
-      height: 3,
-    },
-    shadowOpacity: 5,
-    shadowRadius: 6,
-
-    elevation: 3,
-    borderColor: "white",
-    borderWidth: 2,
-  },
-  pic: {
-    height: 100,
-    width: 100,
-    borderRadius: 100,
-  },
+ 
 });
